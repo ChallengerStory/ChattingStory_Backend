95702ca65d41936cbd93c8fff0c1c93b5e3fdee1
1etterh <1etterh.dev@gmail.com>
2025-02-24 20:15:54 +0900

    feat: Spring Stomp WebSocket 문서 클론 코딩

---
+++ src/main/java/com/challengerstory/chattingstory/dto/Greeting.java
@@ -0,0 +1,9 @@
+package com.challengerstory.chattingstory.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@AllArgsConstructor
+@Data
+public class Greeting {
+    private String content;
+}
