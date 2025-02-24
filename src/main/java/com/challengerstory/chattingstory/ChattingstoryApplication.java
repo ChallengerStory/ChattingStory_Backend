56a4964657cd7435fab85cc51f211dc5d142416a
1etterh <1etterh.dev@gmail.com>
2025-02-24 19:44:26 +0900

    initial commit

---
+++ src/main/java/com/challengerstory/chattingstory/ChattingstoryApplication.java
@@ -0,0 +1,13 @@
+package com.challengerstory.chattingstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingstoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattingstoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challengerstory/chattingstory/ChattingstoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challengerstory.chattingstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingstoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
