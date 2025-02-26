1e4846c52d7b731e1fa75481d47bbef8673e40d8
1etterh <1etterh.dev@gmail.com>
2025-02-26 15:10:48 +0900

    feat: email check

--- src/main/java/com/challengerstory/chattingstory/user/command/application/controller/AuthController.java
+++ src/main/java/com/challengerstory/chattingstory/user/command/application/controller/AuthController.java
@@ -2,29 +2,33 @@ package com.challengerstory.chattingstory.user.command.application.controller;
 
 import com.challengerstory.chattingstory.common.ResponseDTO;
 import com.challengerstory.chattingstory.user.command.application.service.AuthUserService;
+import com.challengerstory.chattingstory.user.command.application.service.EmailVerificationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
     private final AuthUserService authUserService;
-
+    private final EmailVerificationService emailVerificationService;
     @GetMapping("/refresh")
     public ResponseDTO<?> refreshToken(){
         return ResponseDTO.ok("AT 재발급 성공");
     }
 
     @PostMapping("/email-check")
-    public ResponseDTO<?> checkEmail(@RequestBody String email){
+    public ResponseDTO<?> checkEmail(@RequestParam String email){
+        log.debug("email: {}", email);
         return ResponseDTO.ok(authUserService.isEmailAvailable(email));
     }
 
     @PostMapping("/send-verification")
-    public ResponseDTO<?> sendVerificationCode(@RequestBody String email){
-
+    public ResponseDTO<?> sendVerificationCode(@RequestParam String email){
+        log.debug("email: {}", email);
+        emailVerificationService.sendVerificationCode(email);
         return ResponseDTO.ok(email+"이메일 전송 완료");
     }
 
